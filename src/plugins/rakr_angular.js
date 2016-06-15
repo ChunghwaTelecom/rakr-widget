683e8f3cdeef4cdbbad7c0c11d4ae889eba33854
Hsiu-Yuan Shan <hyshan@cht.com.tw>
2016-06-15 16:13:42 +0800

    Add exceptionHandler.

--- src/plugins/rakr_angular.js
+++ src/plugins/rakr_angular.js
@@ -4,7 +4,15 @@
   if (!angular) return;
 
   angular.module('ngRakr', [])
-    .config(['$provide', logProvider]);
+    .config(['$provide', logProvider])
+    .factory('$exceptionHandler', ['$log'], exceptionHandler);
+    
+  function getRakr() {
+    var rakr = window['RakrWidgetObject'];
+    if (rakr) {
+      return window[rakr];
+    }
+  }
 
   function logProvider($provide) {
     $provide.decorator('$log', ['$delegate', logDecorator]);
@@ -12,17 +20,29 @@
 
   function logDecorator($delegate) {
     var functionMap = { log: 0, info: 1, warn: 3 };
-    var rakr = window['RakrWidgetObject'];
-    for (var func in functionMap) {
-      var original = $delegate[func];
-      $delegate[func] = function decoratedLog(msg) {
-        if (window[rakr] && window[rakr]['log']) {
-          window[rakr]['log'](func[func], arguments);
+    var rakr = getRakr();
+    if (rakr) {
+      for (var func in functionMap) {
+        var original = $delegate[func];
+        $delegate[func] = function decoratedLog(msg) {
+          if (rakr['log']) {
+            rakr['log'](func[func], arguments);
+          };
+          original.apply($delegate, arguments);
         };
-        original.apply($delegate, arguments);
-      };
+      }
     }
     return $delegate;
+  }
+
+  function exceptionHandler($log) {
+    return function rakrExceptionHandler(exception, cause) {
+      var rakr = getRakr();
+      if (rakr) {
+        rakr['logError'](exception);
+      }
+      $log.error(exception, cause);
+    };
   }
 
 })();
