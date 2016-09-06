a5267ce2c18a3b18394701cecb634ef8cac56ad5
Pei-Tang Huang <tangtheone@gmail.com>
2016-09-06 14:34:17 +0800

    Change to per-user notification channel.

--- src/lib/widget.ts
+++ src/lib/widget.ts
@@ -41,7 +41,7 @@ export class Widget {
     this.widgetPanel.reportButtonOnClick(() => this.reportIssue());
 
     this.loginPanel.isLoggedIn().then(
-      () => this.updateNotification(),
+      () => this.registerNotification(),
       () => {
         this.widgetPanel.loginButtonShow();
         this.widgetPanel.loginButtonOnClick(
@@ -52,7 +52,7 @@ export class Widget {
               (user) => {
                 this.context.user = user;
                 this.widgetPanel.loginButtonHide();
-                this.updateNotification();
+                this.registerNotification();
               },
               (message) => Prompter.prompt(message)
             )
@@ -73,21 +73,28 @@ export class Widget {
     );
 
     this.reporter = new Reporter(this.context);
+  }
 
+  private registerNotification() {
     let webSocketRoot = document.head.querySelector('[name=backend-websocket-root]') ?
       (<HTMLMetaElement>document.head.querySelector('[name=backend-websocket-root]')).content :
       this.context.resolveFullPath('/websocket/issues');
     this.socket = new SockJS(webSocketRoot);
     let stompClient = Stomp.over(this.socket);
     let _self = this;
-    stompClient.connect({}, function (frame) {
-      stompClient.subscribe('/topic/issues',
-        // TODO since we cannot filter message by user right now,
-        //      issueing an full query again.
-        // FIXME popup notification message (related to user).
-        result => _self.updateNotification()
+    stompClient.connect({}, function (sessionId) {
+      stompClient.subscribe(`/user/${_self.context.user.redmineId}/issues`,
+        (result) => {
+          let obj = JSON.parse(result.body);
+          let created = obj.author;
+          let related = obj.assignee;
+
+          _self.widgetPanel.setCreatedIssuesCount(created);
+          _self.widgetPanel.setRelatedIssuesCount(related);
+        }
       );
     });
+    this.updateNotification();
   }
 
   private updateNotification() {
